9978793758ff83a9e8a8e6213250773fcefc8a52
Dzianis Katsuk <Dzianis_Katsuk@epam.com>
2020-08-03 22:29:47 +0300

    Add table meals to jsp, refactor.

--- src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
@@ -2,21 +2,28 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
 
 
 public class MealsServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(MealsServlet.class);
+    private List<MealTo> meals = MealsUtil.filteredByStreams(MealsUtil.meals, LocalTime.of(0, 0), LocalTime.of(23, 59), 2000);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logger.debug("redirect to meals");
+        logger.debug("forward to meals");
 
-        resp.sendRedirect("meals.jsp");
+        req.setAttribute("meals", meals);
+        req.getRequestDispatcher("/meals.jsp").forward(req, resp);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,12 +1,34 @@
 <%@ page contentType="text/html;charset=UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
-<body>
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+<br>
+<table>
+    <thead style="border: 2px">
+    <tr>
+        <th>Description</th>
+        <th>Date</th>
+        <th>Calories</th>
+        <th>Exceeded</th>
+    </tr>
+
+    </thead>
+    <c:forEach var="meal" items="${requestScope.meals}">
+        <tbody style="${meal.excess ? "background-color: red" : "background-color: green"}">
+        <tr>
+            <th>${meal.description}</th>
+            <th>${meal.dateTime}</th>
+            <th>${meal.calories}</th>
+            <th>${meal.excess}</th>
+        </tr>
+        </tbody>
+    </c:forEach>
+</table>
 </body>
 </html>
