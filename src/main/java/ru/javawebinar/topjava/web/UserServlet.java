b2f2533e717a51965c1dfa71b53ab2478d3dd893
Dzianis Katsuk <Dzianis_Katsuk@epam.com>
2020-08-07 22:37:51 +0300

    Optional done.

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -14,6 +14,16 @@ public class UserServlet extends HttpServlet {
     private static final Logger log = getLogger(UserServlet.class);
 
     @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userIdStr = req.getParameter("userId");
+        int userId = Integer.parseInt(userIdStr);
+
+        SecurityUtil.setUserId(userId);
+
+        resp.sendRedirect("/topjava/index.html");
+    }
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("forward to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
